9af174a3aad1485dd5d905e2e70dd6e0ab71cb2b
caldwella2 <caldwella2@allegheny.edu>
2021-05-05 16:35:27 -0400

    added is_test_valid

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 """ Test Cases """
 
-""" Test case to see if the URL is a valid one """
+import validators
+
+def is_test_valid():
+    """ Test case to see if the URL is a valid one """
+    valid=validators.url()
+    if valid==True:
+        print("Valid")
+    else:
+        print("Invalid")
 
 def test_html_to_html():
     """ Test case to look into HTML -> HTML """
@@ -9,4 +17,4 @@ def test_MD_to_html():
     """ Test case to look into MD -> HTML """
 
 def test_RST_to_html():
-    """ Test case to look into RST -> HTML """ 
+    """ Test case to look into RST -> HTML """
