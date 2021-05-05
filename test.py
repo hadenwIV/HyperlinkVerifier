616da3ea74940ea945a43466cd927ffba1289900
caldwella2 <caldwella2@allegheny.edu>
2021-05-05 16:23:59 -0400

    added some potential  test functions to use

---
+++ test.py
@@ -0,0 +1,12 @@
+""" Test Cases """
+
+""" Test case to see if the URL is a valid one """
+
+def test_html_to_html():
+    """ Test case to look into HTML -> HTML """
+
+def test_MD_to_html():
+    """ Test case to look into MD -> HTML """
+
+def test_RST_to_html():
+    """ Test case to look into RST -> HTML """ 
