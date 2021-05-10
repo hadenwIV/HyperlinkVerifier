e5c043d51b3b3a01b5c81dec5fa695d8391069d1
caldwella2 <caldwella2@allegheny.edu>
2021-05-10 16:01:20 -0400

    fixed the test.py structure and added the init.py file

---
+++ test.py
@@ -0,0 +1,13 @@
+""" Test Cases """
+
+from   import
+
+def test_check():
+    """Test case to test the check() function"""
+    if assert check() is True:
+        print('URL works.')
+    else:
+        print('URL does not work.')
+
+def test_troverdurls():
+    """Test case to test the check() function"""
